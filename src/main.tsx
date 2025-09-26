59572193868e8d55c54d5ac15b50fb4d965a4a87
Youzhi Wang <youzhi.wang.1029@outlook.com>
2025-09-26 18:37:56 +0800

    reactor: .

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import "./styles/index.css";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import router from "./router/index.tsx";
-import { ToastProvider } from "./providers/ToastContext.tsx";
+import { HeroUIProvider } from "@heroui/react";
 
 createRoot(document.getElementById("root")!).render(
-  <ToastProvider>
+  <HeroUIProvider className="w-full h-full overflow-hidden">
     <RouterProvider router={router} />
-  </ToastProvider>
+  </HeroUIProvider>
 );
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,105 +1,46 @@
+import { Button, Form, Input } from "@heroui/react";
 import "./SignIn.scss";
-import slytherinImage from "../../assets/images/slytherin.png";
-import { useForm } from "react-hook-form";
-import api from "@/api";
-import { useToast } from "@/hooks/use-toast";
-import { useState } from "react";
-import { useNavigate } from "react-router";
-
-interface SignInForm {
-  username: string;
-  password: string;
-}
+import type { FormEvent } from "react";
 
 function SignIn() {
-  const navigate = useNavigate();
-  const { toast } = useToast();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const [isLoading, setIsLoading] = useState(false);
-
-  /** handle sign in */
-  const handleSignInSubmit = async (data: SignInForm) => {
-    if (isLoading) return;
-
-    setIsLoading(true);
-    try {
-      const token = await api.auth.signIn(data);
-      localStorage.setItem("authToken", token);
-      toast.success("Sign in successful!");
-      navigate({ pathname: "/" });
-    } catch (error) {
-      if (error instanceof Error) {
-        toast.error(error.message);
-      } else {
-        toast.error(error as string);
-      }
-    } finally {
-      setIsLoading(false);
-    }
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const data = Object.fromEntries(
+      new FormData(e.currentTarget as HTMLFormElement)
+    );
+    console.log("Form Data:", data);
   };
 
   return (
-    <div className="sign-in h-full overflow-y-auto bg-gradient-to-br from-green-950 via-emerald-500 to-[#2A623D] bg-[length:200%_200%]">
-      <div className="min-h-full flex flex-col">
-        <div className="w-full flex-none px-4 py-8">
-          <h1 className="text-5xl font-bold text-white">Slytherin</h1>
-          <h2 className="text-xl font-semibold text-white">
-            Welcome to the Slytherin House
-          </h2>
-        </div>
-
-        <div className="w-full flex-grow bg-white/70 rounded-tl-2xl rounded-tr-2xl overflow-y-auto">
-          <img
-            className="logo h-32 my-4 mx-auto"
-            src={slytherinImage}
-            alt="Slytherin House"
+    <div className="sign-in w-full h-full overflow-y-auto">
+      <div className="form p-4">
+        <Form validationBehavior="native" onSubmit={handleSubmit}>
+          <Input
+            name="username"
+            label="USERNAME"
+            labelPlacement="outside"
+            placeholder="Please enter username"
+            isRequired
+            errorMessage="Username is required"
           />
-
-          <form
-            className="form p-6"
-            onSubmit={handleSubmit((data) => {
-              handleSignInSubmit(data as SignInForm);
-            })}
+          <Input
+            name="password"
+            label="PASSWORD"
+            labelPlacement="outside"
+            placeholder="Please enter password"
+            type="password"
+            isRequired
+            errorMessage="Password is required"
+          />
+          <Button
+            className="w-full mt-4"
+            color="primary"
+            variant="solid"
+            type="submit"
           >
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Username</legend>
-              <input
-                type="text"
-                className="input w-full"
-                placeholder="Username"
-                {...register("username", { required: true })}
-              />
-              {errors.username && (
-                <p className="label text-red-600">This field is required</p>
-              )}
-            </fieldset>
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Password</legend>
-              <input
-                type="password"
-                className="input w-full"
-                placeholder="password"
-                {...register("password", { required: true })}
-              />
-              {errors.password && (
-                <p className="label text-red-600">This field is required</p>
-              )}
-            </fieldset>
-            <fieldset className="fieldset mt-4">
-              <button className="btn text-white bg-[#2A623D]">
-                {isLoading ? (
-                  <span className="loading loading-spinner loading-xs"></span>
-                ) : (
-                  <span>Sign In</span>
-                )}
-              </button>
-            </fieldset>
-          </form>
-        </div>
+            Sign In
+          </Button>
+        </Form>
       </div>
     </div>
   );
